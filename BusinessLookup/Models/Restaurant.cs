52be4b54bf38d26d7851ffe64218c392fc00aa22
glenbuck503 <glenbuck503@gmail.com>
2021-04-02 08:22:58 -0700

    add models

---
+++ BusinessLookup/Models/Restaurant.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLookup.Models
+{
+    public class Business
+    {
+        public int BusinessId { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+        [Required]
+        public string Species { get; set; }
+        [Required]
+        [Range(0, 200, ErrorMessage = "Age must be between 0 and 200.")]
+        public int Age { get; set; }
+        [Required]
+        public string Gender { get; set; }
+
+        
+        
+    }
+}
+
