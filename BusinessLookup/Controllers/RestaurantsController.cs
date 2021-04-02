2897f5aa7fcff0ed0ec0acdf70cc8a2a057269b1
glenbuck503 <glenbuck503@gmail.com>
2021-04-02 08:26:55 -0700

    add controllers

---
+++ BusinessLookup/Controllers/RestaurantsController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BusinessLoookup.Models;
+
+
+namespace BusinessLookup.Controllers
+{
+  
+}
---
+++ BusinessLookup/Controllers/ShopsController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BusinessLookuup.Models;
+
+namespace BusinessLookup.Models
+{
+  
+}
