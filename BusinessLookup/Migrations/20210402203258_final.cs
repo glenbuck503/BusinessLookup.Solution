9fddd6633fc9b5958490ce86b5e2f34092f2dade
glenbuck503 <glenbuck503@gmail.com>
2021-04-02 13:34:17 -0700

    wroking swagger with postman database

---
+++ BusinessLookup/Migrations/20210402203258_final.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BusinessLookup.Migrations
+{
+    public partial class final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Shops",
+                keyColumn: "ShopId",
+                keyValue: 2,
+                column: "Number",
+                value: "pdx");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Shops",
+                keyColumn: "ShopId",
+                keyValue: 2,
+                column: "Number",
+                value: "livelinks");
+        }
+    }
+}
