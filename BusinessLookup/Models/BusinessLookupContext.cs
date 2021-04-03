cc4f795c532ed2d4e1eebdfaa47f79fffe32a106
glenbuck503 <glenbuck503@gmail.com>
2021-04-02 23:45:06 -0700

    update README

--- BusinessLookup/Models/BusinessLookupContext.cs
+++ BusinessLookup/Models/BusinessLookupContext.cs
@@ -14,24 +14,21 @@ namespace BusinessLookup.Models
         {
           builder.Entity<Shop>()
           .HasData(
-             new Shop { ShopId = 1, Name = "first shop", Goods = "yeehaw", Number = "1900"},
-             new Shop { ShopId = 2, Name = "second shop", Goods = "second", Number = "pdx"},
-             new Shop { ShopId = 3, Name = "third shop", Goods = "third", Number = "goodbye c#"}
+            new Shop { ShopId = 1, Name = "first shop", Goods = "yeehaw", Number = "1900"},
+            new Shop { ShopId = 2, Name = "second shop", Goods = "second", Number = "pdx"},
+            new Shop { ShopId = 3, Name = "third shop", Goods = "third", Number = "goodbye c#"}
             
           );
 
           builder.Entity<Restaurant>()
           .HasData(
-             new Restaurant { RestaurantId = 1, Name = "first restaurant", Food = "yeehaw", Number = "555"},
-             new Restaurant { RestaurantId = 2, Name = "second restaurant", Food = "second", Number = "222"},
-             new Restaurant { RestaurantId= 3, Name = "third restaurant", Food = "third", Number = "503"}
+            new Restaurant { RestaurantId = 1, Name = "first restaurant", Food = "yeehaw", Number = "555"},
+            new Restaurant { RestaurantId = 2, Name = "second restaurant", Food = "second", Number = "222"},
+            new Restaurant { RestaurantId= 3, Name = "third restaurant", Food = "third", Number = "503"}
             
           );
         }
 
-  
-    
-    
    
       public DbSet<Shop> Shops { get; set; }
        public DbSet<Restaurant> Restaurants { get; set; }
